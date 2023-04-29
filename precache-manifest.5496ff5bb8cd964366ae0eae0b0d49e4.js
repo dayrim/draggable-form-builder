d7742b0ef951a076001f8a9fbe3b4a614cc26d9d
Dmitrijs Pavlovs <dmitrijs.pavlovs123@gmail.com>
2023-04-29 08:37:42 +0300

    Updates

---
+++ precache-manifest.5496ff5bb8cd964366ae0eae0b0d49e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d7b1d705be7235d5d68d4f29fd5ec74",
+    "url": "/dayrim/draggable-form-builder/index.html"
+  },
+  {
+    "revision": "a48b13d90a2209631760",
+    "url": "/dayrim/draggable-form-builder/static/css/main.8d92131e.chunk.css"
+  },
+  {
+    "revision": "bd04fd926e22a7b207b2",
+    "url": "/dayrim/draggable-form-builder/static/js/2.98375880.chunk.js"
+  },
+  {
+    "revision": "16d1c48ef21e1e175b8d88af654abaed",
+    "url": "/dayrim/draggable-form-builder/static/js/2.98375880.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a48b13d90a2209631760",
+    "url": "/dayrim/draggable-form-builder/static/js/main.866372b6.chunk.js"
+  },
+  {
+    "revision": "fc1ea72aae3d2c1f33c1",
+    "url": "/dayrim/draggable-form-builder/static/js/runtime-main.4674c36b.js"
+  }
+]);
